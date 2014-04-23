fe623962f4a71cd04ea3359fd8736a3f04c5eba6
Cameron <cameronloughman@gmail.com>
2014-04-22 20:05:42 -0700

    yello

--- servers/sched/schedproc.h
+++ servers/sched/schedproc.h
@@ -26,7 +26,6 @@ EXTERN struct schedproc {
 	unsigned flags;		/* flag bits */
 
 	/* User space scheduling */
-	unsigned num_tickets;
 	unsigned max_priority;	/* this process' highest allowed priority */
 	unsigned priority;		/* the process' current priority */
 	unsigned time_slice;		/* this process's time slice */
